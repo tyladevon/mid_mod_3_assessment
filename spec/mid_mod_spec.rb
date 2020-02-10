908f4c42c30d910935e86bbba0040f9c199085a8
Tyla Devon <tyladevon@gmail.com>
2020-02-10 09:32:42 -0700

    Create Spec for Search House Members

--- spec/mid_mod_spec.rb
+++ spec/mid_mod_spec.rb
@@ -1,1 +1,28 @@
 require 'rails_helper'
+
+describe 'As a User, when I visit the dashboard' do
+    describe "When I select 'Gryffindor and click Search'" do
+      it 'I am redirected to a search page and can see the Members of that house' do
+        visit '/'
+        select "Gryffindor", from: :house
+        click_on "Search for Members"
+        expect(current_path).to eq('/search')
+        expect(page).to have_content("21 house members for Gryffindor")
+        expect(page).to have_css(".members, count: 21")
+
+        within(first(".members")) do
+          expect(page).to have_css(".name")
+          expect(page).to have_css(".role")
+          expect(page).to have_css(".house")
+          expect(page).to have_css(".patronus")  
+        end
+      end
+    end
+end
+
+#
+# And for each of the members I should see:
+# - The name of the member
+# - The members role (if it exists)
+# - The house the member belongs to
+# - The Patronus of the member (if it exists)
